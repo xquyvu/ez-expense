b687253b7cd49af1b204613c3d47715a7c238ba5
Quy Vu <40758906+xquyvu@users.noreply.github.com>
2025-09-16 01:27:30 +0200

    mock invoice extraction

--- invoice_extractor.py
+++ invoice_extractor.py
@@ -1,2 +1,2 @@
 def extract_invoice_details() -> dict:
-    return {"Amount": 100.0, "Date": "2024-01-01", "Expense category": "Airfare"}
+    return {"Amount": 100.0, "Currency": "CHF", "Date": "2024-01-01", "Expense category": "Airfare"}
